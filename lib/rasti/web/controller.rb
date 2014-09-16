f8a333ec00e860f4f6939a543d3e93b4c2bbe939
Gabriel Naiman <gnaiman@keepcon.com>
2014-09-16 11:38:10 -0300

    Controller exceptions handling

--- lib/rasti/web/controller.rb
+++ lib/rasti/web/controller.rb
@@ -15,16 +15,32 @@ module Rasti
         @render = render
       end
 
+      def execute(action_name)
+        public_send action_name
+      rescue => ex
+        if respond_to? ex.class.name
+          public_send ex.class.name, ex
+        else
+          raise ex
+        end
+      end
+
       def self.action(action_name)
         raise "Undefined action #{action_name} in #{name}" unless instance_methods.include? action_name.to_sym
         
         Endpoint.new do |req, res, render|
-          self.new(req, res, render).public_send(action_name)
+          self.new(req, res, render).execute(action_name)
         end
       end
 
       def self.>>(action_name)
         action action_name
+      end
+
+      def self.rescue_from(exception_class, &block)
+        define_method exception_class.name do |ex|
+          instance_exec ex, &block
+        end
       end
 
     end
