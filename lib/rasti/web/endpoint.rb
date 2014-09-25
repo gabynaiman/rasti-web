8494043c2ba4f9d16592f5701fc3fe80e6c5ea5b
Gabriel Naiman <gnaiman@keepcon.com>
2014-09-25 18:16:59 -0300

    Merge body params

--- lib/rasti/web/endpoint.rb
+++ lib/rasti/web/endpoint.rb
@@ -8,6 +8,8 @@ module Rasti
 
       def call(env)
         request = Rack::Request.new env
+        body = request.body.gets
+        request.params.merge! JSON.parse(body) if body
         request.params.merge! env[PATH_PARAMS]
         response = Rack::Response.new
         @block.call request, response, Render.new(request, response)
